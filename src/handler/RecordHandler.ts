fb96305e43db93972c740743b53772a9311b38a9
Benny Neugebauer <bn@bennyn.de>
2019-06-03 18:17:52 +0200

    feat: Render jpeg images

--- src/handler/RecordHandler.ts
+++ src/handler/RecordHandler.ts
@@ -1,25 +1,37 @@
 import {MessageHandler} from '@wireapp/bot-api';
 import {PayloadBundle, PayloadBundleType} from '@wireapp/core/dist/conversation/';
-import {FileContent, FileMetaDataContent, TextContent} from '@wireapp/core/dist/conversation/content';
+import {
+  AssetContent,
+  FileContent,
+  FileMetaDataContent,
+  RemoteData,
+  TextContent
+} from '@wireapp/core/dist/conversation/content';
 import {Decoder, Encoder} from 'bazinga64';
 import fs from 'fs-extra';
 import mime from 'mime-types';
-import moment = require('moment');
 import path from 'path';
 import {MessageEntity} from '../entity/MessageEntity';
+import moment = require('moment');
 
 const Printer = require('pdfmake');
 
 class RecordHandler extends MessageHandler {
   async recordText(payload: PayloadBundle): Promise<void> {
     if (this.account && this.account.service) {
-      const user = await this.account.service.user.getUsers([payload.from]);
       const text = (payload.content as TextContent).text;
       const encoded = Encoder.toBase64(text);
+      await this.recordMessage(payload, 'text/plain', encoded.asString);
+    }
+  }
+
+  async recordMessage(payload: PayloadBundle, contentType: string, contentAsBase64: string): Promise<void> {
+    if (this.account && this.account.service) {
+      const user = await this.account.service.user.getUsers([payload.from]);
 
       const messageEntity = new MessageEntity();
-      messageEntity.contentBase64 = encoded.asString;
-      messageEntity.contentType = 'text/plain';
+      messageEntity.contentBase64 = contentAsBase64;
+      messageEntity.contentType = contentType;
       messageEntity.conversationId = payload.conversation;
       messageEntity.messageId = payload.id;
       messageEntity.sendingUserId = payload.from;
@@ -33,7 +45,7 @@ class RecordHandler extends MessageHandler {
   }
 
   async constructContent(): Promise<string[]> {
-    const contents: string[] = [];
+    const contents: any[] = [];
     const messages: MessageEntity[] = await MessageEntity.getRepository().find();
 
     for (const message of messages) {
@@ -43,6 +55,11 @@ class RecordHandler extends MessageHandler {
 
         contents.push(`${message.sendingUserName} on ${time}`);
         contents.push(plainText.asString);
+      } else if (message.contentType === 'image/jpeg') {
+        contents.push({
+          image: `data:${message.contentType};base64,${message.contentBase64}`,
+          width: 200
+        });
       }
     }
 
@@ -168,10 +185,19 @@ class RecordHandler extends MessageHandler {
 
   async handleEvent(payload: PayloadBundle): Promise<void> {
     switch (payload.type) {
+      case PayloadBundleType.ASSET_IMAGE:
+        if (this.account && this.account.service) {
+          const imagePayload = payload.content as AssetContent;
+          const {uploaded, original} = imagePayload;
+          const imageArray = await this.account.service.conversation.getAsset(uploaded as RemoteData);
+          const base64Data = Encoder.toBase64(imageArray).asString;
+          await this.recordMessage(payload, 'image/jpeg', base64Data);
+        }
+        break;
       case PayloadBundleType.TEXT:
-        const content = payload.content as TextContent;
+        const textPayload = payload.content as TextContent;
 
-        if (content.text === '/export') {
+        if (textPayload.text === '/export') {
           const base64Pdf = await this.generatePdf();
           await this.sendAttachment(payload, 'export.pdf', base64Pdf);
         } else {
